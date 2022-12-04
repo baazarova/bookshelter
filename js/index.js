4fce814d9b34f44275c5085cc175d1c5290996a3
baazarova <iskandarovaogiloy@gmail.com>
2022-12-04 17:34:03 +0500

    exam

--- js/index.js
+++ js/index.js
@@ -1,43 +1,36 @@
-let form = document.querySelector('.form');
-let login = document.querySelector('.login-input');
-let password = document.querySelector('.password-input');
+let form = document.querySelector(".form");
+let login = document.querySelector(".login-input");
+let password = document.querySelector(".password-input");
 
 let url = "https://reqres.in/api/login";
-let errorUI = document.createElement('h1');
+let errorUI = document.createElement("h1");
 
-form.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    errorUI.textContent = null;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  errorUI.textContent = null;
 
-    let options = {
-        "method":"POST",
-        "headers":{
-            "Content-type":"application/json",
-        },
-        "body": JSON.stringify({
-            "email": login.value,
-            "password": password.value
-        })
-        
-    };
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      email: login.value,
+      password: password.value,
+    }),
+  };
 
-    async function loginFunc(){
-        let response = await fetch(url, options);
-        let data = await response.json();
-        console.log(data);
-        if(data.token){
-            localStorage.setItem("token", data.token);
-            window.location.replace("../home.html")
-        }
-        else{
-            
-            
-            errorUI.classList = 'error-ui'
-            errorUI.textContent = "User Not Found! Please try again"
-            form.append(errorUI)
-        }
-
+  async function loginFunc() {
+    let response = await fetch(url, options);
+    let data = await response.json();
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      window.location.replace("../home.html");
+    } else {
+      errorUI.classList = "error-ui";
+      errorUI.textContent = "User Not Found! Please try again";
+      form.append(errorUI);
     }
-    loginFunc();
-
-})
+  }
+  loginFunc();
+});
